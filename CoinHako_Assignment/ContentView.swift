c4ba440519fbf8b86e0515c0e3bd7efe7e4a127e
dathtcheapgo <datht@cheapgo.com>
2021-06-06 12:40:42 +0700

    Initial Commit

---
+++ CoinHako_Assignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinHako_Assignment
+//
+//  Created by Tien Dat on 6/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
